fba0f66ea1fa082fa2c1e2d81123d504d5f45712
Mohammed Zeeshan <34814964+zeshhaan@users.noreply.github.com>
2020-12-03 17:30:58 +0530

    Added searchbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 const App = () => {
-  return <div> Marhaba! </div>;
+  return (
+    <div>
+      {" "}
+      <SearchBar />{" "}
+    </div>
+  );
 };
 
 export default App;
